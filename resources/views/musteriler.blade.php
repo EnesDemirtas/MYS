901f26a52bc25fa84f3dfa20e7ee57d241a53828
Enes Demirtaş <enesdemirtas255@gmail.com>
2022-12-01 10:21:20 +0300

    Update musteriler.blade.php

--- resources/views/musteriler.blade.php
+++ resources/views/musteriler.blade.php
@@ -143,12 +143,12 @@
                   </thead>
                   <tbody>
                     <tr>
-                      <td>Tiger Nixon</td>
-                      <td>System Architect</td>
-                      <td>Edinburgh</td>
-                      <td>61</td>
-                      <td>2011/04/25</td>
-                      <td>$170,750</td>
+                      <td>Bim A.Ş.</td>
+                      <td>Caner SARI</td>
+                      <td>Satış Sorumlusu</td>
+                      <td>Döşemealtı/Antalya</td>
+                      <td>25.04.2021</td>
+                      <td>80</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -174,12 +174,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Garrett Winters</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>63</td>
-                      <td>2011/07/25</td>
-                      <td>$170,750</td>
+                      <td>Kaya Gıda Ltd. Şti.</td>
+                      <td>Sude YILDIZ</td>
+                      <td>Kalite Kontrol Sorumlusu</td>
+                      <td>Yalvaç/Isparta</td>
+                      <td>25.07.2022</td>
+                      <td>1</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -205,12 +205,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Ashton Cox</td>
-                      <td>Junior Technical Author</td>
-                      <td>San Francisco</td>
-                      <td>66</td>
-                      <td>2009/01/12</td>
-                      <td>$86,000</td>
+                      <td>Örnek İnşaat</td>
+                      <td>Emrah KURT</td>
+                      <td>Kıdemli İnşaat Mühendisi</td>
+                      <td>Muratpaşa/Antalya</td>
+                      <td>12.01.2020</td>
+                      <td>0</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -236,12 +236,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Cedric Kelly</td>
-                      <td>Senior Javascript Developer</td>
-                      <td>Edinburgh</td>
-                      <td>22</td>
-                      <td>2012/03/29</td>
-                      <td>$433,060</td>
+                      <td>İpragaz</td>
+                      <td>Ahmet GÜLER</td>
+                      <td>Satış Temsilcisi</td>
+                      <td>Konyaaltı/Antalya</td>
+                      <td>29.03.2021</td>
+                      <td>59</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -267,12 +267,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Airi Satou</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>33</td>
-                      <td>2008/11/28</td>
-                      <td>$162,700</td>
+                      <td>Sarkuysan</td>
+                      <td>Yağmur ÜNAL</td>
+                      <td>Kalite Kontrol Mühendisi</td>
+                      <td>Manavgat/Antalya</td>
+                      <td>28.11.2016</td>
+                      <td>15</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -298,12 +298,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Brielle Williamson</td>
-                      <td>Integration Specialist</td>
-                      <td>New York</td>
-                      <td>61</td>
-                      <td>2012/12/02</td>
-                      <td>$372,000</td>
+                      <td>Unilever</td>
+                      <td>Mustafa ÖZKAN</td>
+                      <td>Antalya Bölge Müdürü</td>
+                      <td>Merkez/Antalya</td>
+                      <td>02.12.2018</td>
+                      <td>120</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -329,12 +329,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Herrod Chandler</td>
-                      <td>Sales Assistant</td>
-                      <td>San Francisco</td>
-                      <td>59</td>
-                      <td>2012/08/06</td>
-                      <td>$137,500</td>
+                      <td>ETİ</td>
+                      <td>Emel YILMAZ</td>
+                      <td>Yönetici Asistanı</td>
+                      <td>Bucak/Burdur</td>
+                      <td>06.08.2017</td>
+                      <td>48</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -360,12 +360,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Rhona Davidson</td>
-                      <td>Integration Specialist</td>
-                      <td>Tokyo</td>
-                      <td>55</td>
-                      <td>2010/10/14</td>
-                      <td>$205,500</td>
+                      <td>Torku</td>
+                      <td>Hakan SELİMOĞLU</td>
+                      <td>Bakım Mühendisi</td>
+                      <td>Meram/Konya</td>
+                      <td>14.10.2022</td>
+                      <td>17</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
@@ -391,12 +391,12 @@
                       </td>
                     </tr>
                     <tr>
-                      <td>Colleen Hurst</td>
-                      <td>Javascript Developer</td>
-                      <td>San Francisco</td>
-                      <td>39</td>
-                      <td>2009/09/15</td>
-                      <td>$205,500</td>
+                      <td>Caymazlar Holding</td>
+                      <td>Melek ÜNAL</td>
+                      <td>Kalite Kontrol Mühendisi</td>
+                      <td>Kemer/Antalya</td>
+                      <td>15.09.2019</td>
+                      <td>2</td>
                       <td>
                         <div class="btn-group">
                           <button type="button" class="btn btn-dark btn-sm">
