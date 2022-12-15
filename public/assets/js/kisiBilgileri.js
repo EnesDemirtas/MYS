c47f63dffa184789e6b91a5a8980c2dff5808fad
Enes Demirtaş <enesdemirtas255@gmail.com>
2022-12-15 23:55:16 +0300

    Calışanlar gözat modalı geliştirildi

--- public/assets/js/kisiBilgileri.js
+++ public/assets/js/kisiBilgileri.js
@@ -2,7 +2,18 @@ function kisiBilgileri(id){
     var element = document.getElementById("calisan-"+id).children;
 
     var isim = $("#calisan-"+id+" p").text();
-    $("#modal-isim").text(isim);
-    
+    $("#modal-isim").text(isim);    
+
+    var lokasyon = $("#calisan-"+id+" .lokasyon").attr('name');
+    $("#modal-lokasyon").text(lokasyon);
+
+    var eposta = $("#calisan-"+id+" .inv-email").text();
+    $("#modal-eposta").text(eposta);
+
+    var telefon = $("#calisan-"+id+" .inv-amount").text();
+    $("#modal-cep").text(telefon);
+
+    var dogumTarihi = $("#calisan-"+id+" .dogum").text();
+    $("#modal-dogum").text(dogumTarihi);
 }
 
