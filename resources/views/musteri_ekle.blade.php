abd833061808fee3b189e79af49684c44d213351
stguner <gunersuleymanturker@gmail.com>
2023-01-21 00:53:27 +0300

    musteriler -> haritalar

--- resources/views/musteri_ekle.blade.php
+++ resources/views/musteri_ekle.blade.php
@@ -239,6 +239,8 @@
                                                         <input id="hk-boylam" type="hidden" name="mboylam" placeholder="Boylam" class="form-control" value="{{ old('mboylam') }}">
                                                       </div>
                                                 </div>
+                                                <div role="button" id="anlikKonum" class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#de1717" version="1.1" id="Capa_1" width="800px" height="800px" viewBox="0 0 395.71 395.71" xml:space="preserve" stroke="#006eff"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <g> <path d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738 c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388 C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191 c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z"/> </g> </g></svg>
+                                                </div>
                                                 <div class="py-3 row">
                                                     <div class="col-12" id="map"> <!-- GOOGLE HARİTALAR -->
                                                 </div>
@@ -279,16 +281,6 @@
           $("#hk-boylam").attr("value",boylam);    
         }
         let map, infoWindow;
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-          infoWindow.setPosition(pos);
-          infoWindow.setContent(
-            browserHasGeolocation
-              ? "Error: Konum bilgileri alınamadı"
-              : "Error: Tarayıcınız konum bilgilerinin alınmasına izin vermedi."
-          );
-          infoWindow.open(map);
-        }
-      
           function initMap() {
             map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 36.89241570427338, lng: 30.710640679285348 },
@@ -297,10 +289,8 @@
   infoWindow = new google.maps.InfoWindow();
   let marker = new google.maps.Marker();
 
-  const locationButton = document.createElement("button");
-  locationButton.setAttribute("role", "button");
+  const locationButton = document.getElementById("anlikKonum");
 
-  locationButton.textContent = "Bulunduğunuz konumu almak için tıklayınız.";
   locationButton.classList.add("custom-map-control-button");
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
   locationButton.addEventListener("click", () => {
@@ -321,14 +311,8 @@
           enlemBoylamDegis(pos.lat,pos.lng); // enlem ve boylam bilgilerini inputa verir.
           marker.setMap(map);
           map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
         }
       );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
     }
   });
 
