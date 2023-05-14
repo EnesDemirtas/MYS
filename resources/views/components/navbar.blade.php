38c75cea24d87ee425c25b8f89968290de123baa
stguner <gunersuleymanturker@gmail.com>
2023-05-14 10:13:54 +0300

    Navbar açıldı

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,7 +13,7 @@
 
         <ul class="list-unstyled menu-categories" id="topAccordion">
 
-            <li class="menu single-menu d-none">
+            <li class="menu single-menu">
                 <a href="/"  aria-expanded="false" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -45,7 +45,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu d-none">
+            <li class="menu single-menu">
                 <a href="/randevu_yonetimi"  aria-expanded="false" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
@@ -71,7 +71,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu d-none">
+            <li class="menu single-menu">
                 <a href="/gorev_yonetimi"  aria-expanded="false" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -89,7 +89,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu d-none">
+            <li class="menu single-menu">
                 <a href="/teklifler"  aria-expanded="false" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -107,7 +107,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu d-none">
+            <li class="menu single-menu">
                 <a href="/on_muhasebe"  aria-expanded="false" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -125,7 +125,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu d-none">
+            <li class="menu single-menu">
                 <a href="/hizmet_ve_urunler"  aria-expanded="false" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
