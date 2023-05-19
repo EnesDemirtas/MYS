963695ae57e548778f0bb2c1a61e17b0fc77d7c2
Enes Demirtaş <enesdemirtas255@gmail.com>
2023-05-19 14:24:00 +0300

    Correct functions places
    Move ActivationCode function from UserController to ActivationCodeController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ResetPasswordActivationCode;
 use Illuminate\Http\Request;
 use App\Models\calisan;
-use App\Models\ActivationCode;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -86,68 +83,8 @@ class UserController extends Controller
         return redirect()->route('profile', ['kullanici' => $kullanici]);
     }
 
-    public function SendResetActivationCode(Request $request) {
-        $request->validate([
-            'email' => 'required|email'
-        ], [
-            'email.required' => 'E-posta boş bırakılamaz',
-            'email.email' => 'Geçerli bir e-posta adresi giriniz'
-        ]);
-
-        $kullanici = calisan::where('ceposta', $request->input('email'))->first();
-        if ($kullanici) {
-            $activation_code = rand(100000, 999999);
-            $expires_at = now()->addMinutes(5);
-            $activationcode = ActivationCode::create([
-                'eposta' => $kullanici->ceposta,
-                'aktivasyonkodu' => $activation_code,
-                'sure' => $expires_at
-            ]);
-
-            Mail::to($kullanici->ceposta)->send(new ResetPasswordActivationCode($activationcode));
-            
-            // return view('sifre_yenileme_kod', ['aktivasyonkodu' => $activationcode]);
-            // dd($activationcode->eposta);
-            return redirect()->route('get_new_password', ['eposta' => $activationcode->eposta]);
-        } else {
-            return back()->withErrors(['email' => 'E-posta adresi bulunamadı!'])->onlyInput('email');
-        }
-    }
-
     public function GetNewPassword(Request $request) {
         return view('sifre_yenileme_kod', ['eposta' => $request->query('eposta')]);
     }
 
-    public function NewPassword(Request $request) {
-        $request->validate([
-            'aktivasyonkodu' => 'required',
-            'password' => 'required|confirmed|min:6'
-        ], [
-            'aktivasyonkodu.required' => 'Aktivasyon kodu boş bırakılamaz',
-            'password.required' => 'Şifre boş bırakılamaz',
-            'password.confirmed' => 'Şifreler eşleşmiyor!',
-            'password.min' => 'Şifre en az 6 karakter olmalıdır!'
-        ]);
-
-        $activationcode = ActivationCode::where('eposta', $request->email)->orderBy('created_at', 'desc')->first();
-        if ($activationcode->sure < now()) {
-            $activation_code = rand(100000, 999999);
-            $expires_at = now()->addMinutes(5);
-            $activationcode = ActivationCode::create([
-                'eposta' => $request->email,
-                'aktivasyonkodu' => $activation_code,
-                'sure' => $expires_at
-            ]);
-
-            Mail::to($request->email)->send(new ResetPasswordActivationCode($activationcode));
-            return back()->withErrors(['aktivasyonkodu' => 'Aktivasyon kodu süresi doldu! Yeni kod mail adresinize gönderildi.'])->onlyInput('aktivasyonkodu');
-        } else if ($request->aktivasyonkodu != $activationcode->aktivasyonkodu) {
-            return back()->withErrors(['aktivasyonkodu' => 'Aktivasyon kodu hatalı!']);
-        } else {
-            $kullanici = calisan::where('ceposta', $request->email)->first();
-            $kullanici->update(['csifre' => $request->password]);
-            // $activationcode->delete();
-            return redirect()->route('giris_yap')->with('sifre_degistirme_basarili', 'Şifreniz başarıyla değiştirildi!');
-        }
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -111,6 +111,6 @@ Route::post('kayit_ol', [UserController::class, 'register'])->name('register');
 Route::get('profile', [UserController::class, 'GetProfile'])->name('profile');
 Route::post('uploadPP', [UserController::class, 'UploadPP'])->name('uploadPP');
 
-Route::post('reset_activation_code', [UserController::class, 'SendResetActivationCode'])->name('reset_activation_code');
+Route::post('reset_activation_code', [ActivationCodeController::class, 'SendResetActivationCode'])->name('reset_activation_code');
 Route::get('new_password', [UserController::class, 'GetNewPassword'])->name('get_new_password');
-Route::post('new_password', [UserController::class, 'NewPassword'])->name('new_password');
+Route::post('new_password', [ActivationCodeController::class, 'NewPassword'])->name('new_password');
